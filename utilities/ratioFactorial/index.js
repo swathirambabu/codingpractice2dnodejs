88edd8900338bc8a390faa7355888caefdc2cfd8
swathi <uttaralaswathi1998@gmail.com>
2023-12-25 09:20:39 +0000

    2d

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -1,8 +1,9 @@
 const factorialOfNumber = require("../factorial/index");
 const ratioOfTwoNumbers = require("../ratio/index");
 const ratioAndFactorial = (num1, num2, num3) => {
-  const factorial = factorialOfNumber(num1);
-  const ratio = ratioOfTwoNumbers(num2, num3);
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num3);
+
   return { ration, factorial };
 };
 module.exports = ratioAndFactorial;
